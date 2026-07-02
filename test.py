bfe1653213c0c779fbcfad23a1a8297c15cd6180
Husnain5khalid <husnainkhalidkhan6@gmail.com>
2026-07-02 15:05:46 +0300

    Tools Creation has been completed

---
+++ test.py
@@ -0,0 +1,9 @@
+from tools.tavily_tool import tavily_search
+from tools.flight_tool import search_flights
+
+##res = tavily_search("What is AI")
+##print(res)
+
+res = search_flights("Plan a 7 Days Turkey trip from Riyadh")
+print(res)
+
