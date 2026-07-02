49b1da471ec84a447b860aa64468283b30dd941f
Husnain5khalid <husnainkhalidkhan6@gmail.com>
2026-07-02 15:27:56 +0300

    Project Completed

--- test.py
+++ test.py
@@ -4,6 +4,16 @@ from tools.flight_tool import search_flights
 ##res = tavily_search("What is AI")
 ##print(res)
 
-res = search_flights("Plan a 7 Days Turkey trip from Riyadh")
-print(res)
+#res = search_flights("Plan a 7 Days Turkey trip from Riyadh")
+#print(res)
+
+user_input = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
 
